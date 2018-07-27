0c26d168f9c1baf4f6527b7476d9ee9a0f5e2de9
ed1ff <edifei13@yandex.ua>
2018-07-27 12:18:14 +0300

    added task api file added environment file

---
+++ src/api/task.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../environments/environment';
+
+@Injectable()
+export class TaskAPI {
+
+  constructor(public http: HttpClient) {}
+
+  query() {
+    return this.http.get(`${environment.api_endpoint}/tasks`);
+  }
+
+  create(params) {
+    return this.http.post(`${environment.api_endpoint}/tasks`, params);
+  }
+
+  update(id, params) {
+    return this.http.patch(`${environment.api_endpoint}/tasks/${id}`, params);
+  }
+
+  delete(id) {
+    return this.http.delete(`${environment.api_endpoint}/tasks/${id}`);
+  }
+
+  get(id) {
+    return this.http.get(`${environment.api_endpoint}/tasks/${id}`);
+  }
+}
